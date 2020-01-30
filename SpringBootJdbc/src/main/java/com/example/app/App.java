410982dd9785b8b94bd63511935f833213074171
Get <dkdlwmzhs@naver.com>
2020-01-30 17:53:38 +0900

    12

--- SpringBootJdbc/src/main/java/com/example/app/App.java
+++ SpringBootJdbc/src/main/java/com/example/app/App.java
@@ -1,17 +1,36 @@
 package com.example.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import com.example.app.domain.Customer;
 
 @SpringBootApplication
 public class App implements CommandLineRunner{
 
-	
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
 	
 	@Override
 	public void run(String... args) throws Exception {
+		// customers 테이블에서 키를 지정하여 정보를 얻는 SQL를 작성
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		
+		// 파라미터로 기본 키의 값을 설정합니다.
+		SqlParameterSource param = new MapSqlParameterSource()
+				.addValue("id", 1);
+		
+		// ResultSet에서 Customer 객체를 생성하는 RowMapper<Customer>를 구현합니다.
+		// Lambda식 으로 코드를 간소화 합니다.
+		Customer result = jdbcTemplate.queryForObject(sql, param, (rs, rowNum) -> new Customer(
+				rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name")));
+		
+		System.out.println("result = " + result);
 	}
 	
 	public static void main(String[] args) {
---
+++ SpringBootJdbc/src/main/java/com/example/app/AppConfig.java
@@ -0,0 +1,41 @@
+package com.example.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+
+//@Configuration
+public class AppConfig {
+	
+	@Autowired
+	DataSourceProperties dataSrcProp; // 데이터 소스를 명시적으로 C드라이브에 저장 했습니다. 이 속성값 들을 가져옵니다.
+	DataSource dataSrc; // 여기에 위 prop변수에 내포되어 있는 속성값 들을 아래 래핑 작업으로 래핑해줍니다.
+	
+	@Bean
+	DataSource realDataSource() {
+		// 스프링 데이터가 제공하는 Builder클래스로 DataSource 인스턴스를 생성 합니다.
+		DataSourceBuilder factory = DataSourceBuilder
+				.create(this.dataSrcProp.getClassLoader())
+				.url(this.dataSrcProp.getUrl())
+				.username(this.dataSrcProp.getUsername())
+				.password(this.dataSrcProp.getPassword());
+		
+		this.dataSrc = factory.build();
+		return this.dataSrc;
+	}
+	
+	@Bean
+	@Primary
+	DataSource dataSource() {
+		// Log4jdbcProxyDataSource 클래스로 DataSource를 래핑합니다. 이 클래스가 DataSource에 구현된
+		// 각각의 처리에 로깅 처리를 끼워 넣습니다.
+		return new Log4jdbcProxyDataSource(this.dataSrc);
+	}
+}
