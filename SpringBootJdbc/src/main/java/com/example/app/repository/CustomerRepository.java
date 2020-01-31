9eaef02be96fceba0534ee09d07848b6138e1a4b
Get <dkdlwmzhs@naver.com>
2020-01-31 15:50:15 +0900

    123

--- SpringBootJdbc/src/main/java/com/example/app/repository/CustomerRepository.java
+++ SpringBootJdbc/src/main/java/com/example/app/repository/CustomerRepository.java
@@ -2,35 +2,66 @@ package com.example.app.repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.app.domain.Customer;
 
 @Repository
+@Transactional
 public class CustomerRepository {
-	private final ConcurrentMap<Integer, Customer> customerMap = 
-			new ConcurrentHashMap<Integer, Customer>();
+	@Autowired
+	NamedParameterJdbcTemplate jdbcTemplate;
 	
-	// 전부 출력
+	private static final RowMapper<Customer> customerRowMapper = (rs, i) -> {
+		Integer id = rs.getInt("id");
+		String firstName = rs.getString("first_name");
+		String lastName = rs.getString("last_name");
+		return new Customer(id, firstName, lastName);
+	};
+	
+	// Select All Condition Block
 	public List<Customer> findAll() {
-		return new ArrayList<Customer>(customerMap.values());
+		String sql = "SELECT id,first_name,last_name FROM customers ORDER BY id";
+		
+		List<Customer> customers = jdbcTemplate.query(
+				sql, customerRowMapper);
+		return customers;
 	}
 	
-	// HashMap 요소 Key값 으로 찾기
+	// Select Condition Block
 	public Customer findOne(Integer customerId) {
-		return customerMap.get(customerId);			
+		String sql = "SELECT id,first_name,last_name FROM customers WHERE id=:id";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", customerId);
+		return jdbcTemplate.queryForObject(sql, param, customerRowMapper);
 	}
 	
-	// HashMap 요소 추가
+	// Update Block
 	public Customer save(Customer customer) {
-		return customerMap.put(customer.getId(), customer);
+		String sql = "INSERT INTO customers(first_name, last_name) ";
+		String values = "values(:firstName, :lastName)";
+		
+		SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
+		if (customer.getId() == null) {
+			jdbcTemplate.update(sql+values, param);
+		}
+		
+		return customer;
 	}
 	
-	// HashMap 요소 삭제
+	// Delete Condition Block
 	public void delete(Integer customerId) {
-		customerMap.remove(customerId);
+		String sql = "DELETE FROM customers WHERE id=:id";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", customerId);
+		jdbcTemplate.update(sql, param);
 	}
 }
